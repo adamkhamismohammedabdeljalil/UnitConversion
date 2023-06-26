3769520da8a94c8127c0fc50a39e98278cff2eaa
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-06-26 13:44:56 +0200

    feat:add UnitConversion project

---
+++ UnitConversion/CelsiusToFahrenheitConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitConversion
+{
+    internal class CelsiusToFahrenheitConverter
+    {
+        public double TempInCelsius
+        {
+            get; set;   
+        }
+        public void Convert()
+        {
+            double result = 1.8 * TempInCelsius + 32;
+            Console.WriteLine($"{TempInCelsius} °C in Fahrenheit is : {result} °F.");
+        }
+    }
+}
---
+++ UnitConversion/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UnitConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Celsius to Fahrenheit converter");
+            CelsiusToFahrenheitConverter convert2Fahrenheit=new CelsiusToFahrenheitConverter();
+            convert2Fahrenheit.TempInCelsius = 25;
+            convert2Fahrenheit.Convert();
+        }
+    }
+}
